6d9ebcabd6c400fe2a8e08c803430106801c9532
Kỳ Lê <kylh84@gmail.com>
2025-03-05 20:23:08 +0700

    Update print messsage

--- src/main/java/edu/r2s/demo/DemoApplication.java
+++ src/main/java/edu/r2s/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.print("Welcome to Spring boot");
 		System.out.print("Hello World");
 		SpringApplication.run(DemoApplication.class, args);
 	}
